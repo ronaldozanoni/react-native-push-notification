84236c076a84073abece52085d19e382e21800cd
Ronaldo Zanoni <phpronaldo@gmail.com>
2018-08-26 16:58:38 -0300

    get message body from 'alert' property too

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -140,7 +140,9 @@ public class RNPushNotificationHelper {
                 return;
             }
 
-            if (bundle.getString("message") == null) {
+            String message = bundle.getString("message") != null ? bundle.getString("message") : bundle.getString("alert");
+
+            if (message == null) {
                 // this happens when a 'data' notification is received - we do not synthesize a local notification in this case
                 Log.d(LOG_TAG, "Cannot send to notification centre because there is no 'message' field in: " + bundle);
                 return;
@@ -173,7 +175,7 @@ public class RNPushNotificationHelper {
                 notification.setGroup(group);
             }
 
-            notification.setContentText(bundle.getString("message"));
+            notification.setContentText(message);
 
             String largeIcon = bundle.getString("largeIcon");
 
